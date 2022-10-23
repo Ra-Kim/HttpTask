e37a2aa3ee5fa91fc2006caf7bb812c8fe3ee7d6
Increase <increasechris124@gmail.com>
2022-10-23 18:12:20 +0100

    Built a http server for displaying static html

--- index.js
+++ index.js
@@ -1,63 +1,58 @@
 const http = require('http')
 const path = require('path')
-const fs = require('fs').promises
+const fs = require('fs')
 const port = 5000
 const host = "localhost"
 
 const server = http.createServer((request,response) => {
-  response.setHeader('Content-Type' , 'text/html')
+  response.setHeader('Content-Type', 'text/html')
   switch(request.url){
-    case './':
+    case '/':
       response.writeHead(200)
-    let ordPath = path.join(__dirname, 'index.html' )
+    let ordPath = path.join(__dirname, 'public', 'index.html' )
     fs.readFile(ordPath, 'utf8', (err, data) => {
-      response.end(data)
+      try{response.end(data)}catch{console.error(err)}
     })
     break
 
-  case './home':
+    case '/home':
       response.writeHead(200)
-    let homePath = path.join(__dirname, 'index.html' )
+    let homePath = path.join(__dirname, 'public', 'index.html' )
     fs.readFile(homePath, 'utf8', (err, data) => {
-      response.end(data)
+      try {response.end(data)} catch {console.error(err)}
     })
     break
 
-  case './about':
+    case '/about':
       response.writeHead(200)
-    let aboutPath = path.join(__dirname, 'about.html' )
+    let aboutPath = path.join(__dirname, 'public', 'about.html' )
     fs.readFile(aboutPath, 'utf8', (err, data) => {
-      response.end(data)
+      try{response.end(data)}catch{console.error(err)}
     })
     break
 
-  case './contact':
+    case '/contact':
       response.writeHead(200)
-    let contactPath = path.join(__dirname, 'about.html' )
+    let contactPath = path.join(__dirname, 'public', 'contact.html' )
     fs.readFile(contactPath, 'utf8', (err, data) => {
-      response.end(data)
+      try{response.end(data)}catch{console.error(err)}
     })
     break
 
     default: 
-      // response.writeHead(404)
-      // response.end(`
-      //   <html>
-      //     <body>
-      //     <h1>Error 404</h1>
-      //       <p>Page Not Found</p>
-      //       <a href = "index.html">Home</a>
-      //     </body>
-      //   </html>
-      // `)
-      response.writeHead(200)
-    let defaultPath = path.join(__dirname, 'index.html' )
-    fs.readFile(defaultPath, 'utf8', (err, data) => {
-      response.end(data)
-    })
+      response.writeHead(404)
+      response.end(`
+        <html>
+          <body>
+          <h1>Error 404</h1>
+            <p>Page Not Found</p>
+            <a href = "home">Home</a>
+          </body>
+        </html>
+      `)
   }
 })
 
-server.listen(port, () => {
-  console.log("server")
+server.listen(port, host, () => {
+  console.log(`server is running on http://${host}:${port}`)
 })
